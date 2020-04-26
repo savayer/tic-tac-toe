1463853eb50f0168b3601fa3b3f6a3bd54cfacd6
savayer <sava0094@gmail.com>
2020-04-26 19:23:24 +0800

    (add) include babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
 const path = require('path');
 const config = {
-    entry: './index.js',    
+    entry: './index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
         publicPath: 'dist/'
     },
     devServer: {
-        overlay: true,        
+        overlay: true,
         contentBase: path.join(__dirname, 'src')
     },
     resolve: {
         alias: {
             __js: path.resolve(__dirname, './src/js/')
         }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: require.resolve("babel-loader"),
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
     }
 }
 
